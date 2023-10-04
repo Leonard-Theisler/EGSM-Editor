c1dcdcb0f9590139c6afaf2b69cb52f08fa70f55
Leonard-Theisler <s204688@dtu.dk>
2023-10-04 17:12:29 +0200

    Implemeted Serializer class and added translator class from Giovanni's project. Created a double click tool to try and call the method, does not work

--- newEditor.design/src/newEditor/design/Services.java
+++ newEditor.design/src/newEditor/design/Services.java
@@ -10,8 +10,12 @@ public class Services {
     /**
     * See http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24 for documentation on how to write service methods.
     */
+    Translator translator;
+
     public EObject myService(EObject self, String arg) {
-       // TODO Auto-generated code
+    	System.out.println("sflghsdæl");
       return self;
     }
+    
+	
 }
---
+++ newEditor.design/src/newEditor/design/Translator.java
@@ -0,0 +1,100 @@
+package newEditor.design;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+//import org.eclipse.m2m.atl.common.ATLExecutionException;
+//import org.eclipse.m2m.atl.core.ATLCoreException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+/*This code was written by Giovanni Meroni and I have received permission to reuse it for this project
+ *The code can be found here: https://bitbucket.org/polimiisgroup/egsmengine/src/master/  
+ */
+
+public class Translator {
+	
+
+	public String getProcessName(String filename){
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder;
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(new File(filename));
+        doc.getDocumentElement().normalize();
+        return doc.getDocumentElement().getElementsByTagName("bpmn2:process").item(0).getAttributes().getNamedItem("id").getNodeValue();
+
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return filename;
+	}
+	
+	/*
+	 * inFilename is the name (including path) of the xmi
+	 * outFilename is the name (including path) of the outputed xml file
+	 * xslFilename is the name (including path) of the xsl file (xmi to xml convention)
+	 * C:\Users\leona\OneDrive\Bureau\Thesis\bpmn2egsm\it.polimi.isgroup.bpmn2egsmplugin\xmi2siena.xsl
+	 */
+	public void transformXML(String inFilename, String outFilename, String xslFilename){
+		try {
+            // Create transformer factory
+            TransformerFactory factory = TransformerFactory.newInstance();
+
+            // Use the factory to create a template containing the xsl file
+            Templates template = factory.newTemplates(new StreamSource(
+                new FileInputStream(xslFilename)));
+
+            // Use the template to create a transformer
+            Transformer xformer = template.newTransformer();
+
+            // Prepare the input and output files
+            Source source = new StreamSource(new FileInputStream(inFilename));
+            Result result = new StreamResult(new FileOutputStream(outFilename));
+
+            // Apply the xsl file to the source file and write the result to the output file
+            xformer.transform(source, result);
+        } catch (FileNotFoundException e) {
+        	
+        } catch (TransformerConfigurationException e) {
+            // An error occurred in the XSL file
+        } catch (TransformerException e) {
+            // An error occurred while applying the XSL file
+            // Get location of error in input file
+            /*
+        	SourceLocator locator = e.getLocator();
+            int col = locator.getColumnNumber();
+            int line = locator.getLineNumber();
+            String publicId = locator.getPublicId();
+            String systemId = locator.getSystemId();
+            */
+        }
+	}
+	
+}
