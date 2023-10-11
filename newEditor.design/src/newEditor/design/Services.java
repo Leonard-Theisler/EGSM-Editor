23b71ab5da4787f64e736bbc7525eaed617997da
Leonard-Theisler <s204688@dtu.dk>
2023-10-11 11:28:50 +0200

    Deleted double click tool, saving/serialization is now done by right clicking and selecting save->save as xml Implemented file explorer popup to get file path when saving

--- newEditor.design/src/newEditor/design/Services.java
+++ newEditor.design/src/newEditor/design/Services.java
@@ -1,11 +1,16 @@
 package newEditor.design;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
 
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -23,13 +28,14 @@ import org.eclipse.emf.ecore.EObject;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-//import jdk.internal.org.jline.terminal.TerminalBuilder.SystemOutput;
 
 /**
  * The services class used by VSM.
  */
 public class Services {
-    
+	
+	
+	String filePath;
 	String inFile = "C:\\Users\\leona\\git\\EGSM-Editor\\newModel2\\newModel2.gsm_derived";
 	String outFile = "C:\\Users\\leona\\OneDrive\\Bureau\\SerializationTest\\newModel2.xml";
 	String xslFile = "C:\\Users\\leona\\OneDrive\\Bureau\\Thesis\\bpmn2egsm\\it.polimi.isgroup.bpmn2egsmplugin\\xmi2siena.xsl";
@@ -39,19 +45,37 @@ public class Services {
     }
     
     
-    public EObject save(EObject self) {
-    	System.out.println("called");
-    	transformXML(inFile, outFile, xslFile);
-    	System.out.println("terminated");
+    public EObject save(EObject self) throws IOException {
+
+    	getFilePath();
+    	transformXML(inFile, filePath, xslFile);
+    	
 		return self;
     }
+    
+    // Opens the file explorer and gets the filename and path from the user   
+    public String getFilePath() {
+        
+        JFileChooser fileExplorer = new JFileChooser();
+
+        fileExplorer.setDialogTitle("Save to");
+    	FileNameExtensionFilter xmlFilter = new FileNameExtensionFilter("xml files (*.xml)", "xml");
+    	fileExplorer.setFileFilter(xmlFilter);
+    	
+        fileExplorer.showSaveDialog(null);
+        
+        filePath = fileExplorer.getSelectedFile().toString();
+        if (!filePath.contains(".xml")) {filePath += ".xml";}; 
+
+        return filePath;
+   
+   }
     
 	
 	/* This method was implemented by Giovanni Meroni and I have received permission to reuse it for this project
      * The code can be found here: https://bitbucket.org/polimiisgroup/egsmengine/src/master/ 
 	 */
 	public void transformXML(String inFilename, String outFilename, String xslFilename){
-		System.out.println("transformXML()");
 		try {
             // Create transformer factory
             TransformerFactory factory = TransformerFactory.newInstance();
