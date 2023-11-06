2b9011e79b76b8e6501adc913d55272e53ba436b
Leonard-Theisler <s204688@dtu.dk>
2023-11-06 12:23:54 +0100

    Finished refactoring

--- newEditor.design/src/newEditor/design/ModelElementFactory.java
+++ newEditor.design/src/newEditor/design/ModelElementFactory.java
@@ -1,5 +1,6 @@
 package newEditor.design;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import org.eclipse.emf.ecore.EObject;
@@ -9,11 +10,13 @@ import application.ConditionType;
 import application.DataFlowGuardType;
 import application.MilestoneType;
 import application.StageType;
+import application.SubStageType;
 import application.impl.ComponentTypeImpl;
 import application.impl.ConditionTypeImpl;
 import application.impl.DataFlowGuardTypeImpl;
 import application.impl.DataItemTypeImpl;
 import application.impl.EventModelTypeImpl;
+import application.impl.EventTypeImpl;
 import application.impl.GuardedStageModelTypeImpl;
 import application.impl.InformationModelTypeImpl;
 import application.impl.MilestoneTypeImpl;
@@ -24,6 +27,15 @@ public class ModelElementFactory {
 	
 	Checker check = new Checker();
 	UUIDFactory idmaker = new UUIDFactory();
+	
+	int stagenum = 0;
+	int eventnum = 0;
+	int stageIndex;
+	String id;
+	String name;
+	String failure = "You must create a model";
+	ArrayList<String> alreadyGen = new ArrayList<String>();
+	ArrayList<String> eventCheck = new ArrayList<String>();
 	
 	public void EGSMfactory(EObject element) {
 		
@@ -54,7 +66,6 @@ public class ModelElementFactory {
     	
     	populateStage(app);    
     	    	
-    	//return false;
     }
 	
 	private boolean createStage(StageType stage) {
@@ -95,13 +106,12 @@ public class ModelElementFactory {
 
 	    	
 	    	
-	    	System.out.println(app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).getDataFlowGuard());//.setDFG(new DataFlowGuardTypeImpl());
+	    	System.out.println(app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).getDataFlowGuard());
 	    	app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).setDFG(data);
-	    	//System.out.println(app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).getDataFlowGuard());//.setDFG(new DataFlowGuardTypeImpl());
+	    	
 
-	    	System.out.println(app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).getMilestone());//.setDFG(new DataFlowGuardTypeImpl());
+	    	System.out.println(app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).getMilestone());
 	    	app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).setMilestone(mile);
-	    	//System.out.println(app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).getMilestone());//.setDFG(new DataFlowGuardTypeImpl());
 
 	    }
 	 
@@ -126,13 +136,11 @@ public class ModelElementFactory {
 
 	    	
 	    	
-	    	System.out.println(stage.getSubStage().get(stage.getSubStage().size() -1).getDataFlowGuard());//.setDFG(new DataFlowGuardTypeImpl());
+	    	System.out.println(stage.getSubStage().get(stage.getSubStage().size() -1).getDataFlowGuard());
 	    	stage.getSubStage().get(stage.getSubStage().size() -1).setDFG(data);
-	    	//System.out.println(app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).getDataFlowGuard());//.setDFG(new DataFlowGuardTypeImpl());
 
 	    	System.out.println(stage.getSubStage().get(stage.getSubStage().size() -1).getMilestone());
 	    	stage.getSubStage().get(stage.getSubStage().size() -1).setMilestone(mile);
-	    	//System.out.println(app.getComponent().get(0).getGuardedStageModel().getStage().get(app.getComponent().get(0).getGuardedStageModel().getStage().size() -1).getMilestone());//.setDFG(new DataFlowGuardTypeImpl());
 
 	    }
 	 
@@ -160,4 +168,280 @@ public class ModelElementFactory {
 	    	app.setEventModel(new EventModelTypeImpl());
 	    	
 	    }
+	 
+	 public void createEvent(CompositeApplicationType app) {
+	    	
+	    	EventTypeImpl event = new EventTypeImpl();
+	    	
+	    	id = UUID.randomUUID().toString().substring(0,4);
+	    	event.setId("Event " + id);
+
+	    	
+	    	app.getEventModel().setEvent(event);     	
+	    	
+	    }
+	    
+	public void makeEvents(CompositeApplicationType app) {
+	    	
+	    	ArrayList<String> dupeCheck = new ArrayList<String>();
+	    	
+	    	generateEvents(app);
+	    	
+	    	   	
+	    	if (alreadyGen.size() > 0) {
+	    		//eventcheck is the new events
+	    		//alreadygen is all of the events
+	    		   		
+	    		for (int j = 0; j < alreadyGen.size(); j++) {
+	        		if ( !eventCheck.contains(alreadyGen.get(j))) {
+	        			for (int k = 0; k < app.getEventModel().getEvent().size(); k++) {
+	        				if (app.getEventModel().getEvent().get(k).getId() == alreadyGen.get(j)) {
+	        					app.getEventModel().getEvent().remove(k);
+	        					alreadyGen.remove(j);
+	        				}
+	        			}
+	        		}
+	        	}
+	    		
+	    	}
+	    	
+			removeDupes(app, dupeCheck);
+
+	    }
+	    
+	    public void removeDupes(CompositeApplicationType app, ArrayList<String> list) {
+	    	for (int i = 0; i < app.getEventModel().getEvent().size(); i++) {
+				if (!list.contains(app.getEventModel().getEvent().get(i).getId())){
+					list.add(app.getEventModel().getEvent().get(i).getId());
+				}
+				else {
+					app.getEventModel().getEvent().remove(i);
+				}
+				if (app.getEventModel().getEvent().get(i).getId() == "") {
+					app.getEventModel().getEvent().remove(i);
+				}
+			}
+	    }
+	    
+	    public void generateEvents(CompositeApplicationType app) {
+	    	
+	    	eventCheck.clear();
+	    	
+	    	int stages = app.getComponent().get(0).getGuardedStageModel().getStage().size();
+	    	
+	    	if (stages == 0) {
+	    		app.getEventModel().getEvent().clear();
+	    	}
+	    	
+	    	for (int i = 0; i < stages; i++) {
+	    		for (int j = 0; j < app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getMilestone().size(); j++) {
+	    			if (app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getMilestone().get(j).getEventIds() != null) {
+	    				
+	    				String eventID = app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getMilestone().get(j).getEventIds();
+	    				
+	    				eventCheck.add(eventID);
+
+	    				if (!alreadyGen.contains(eventID)) {
+	    					
+	    					EventTypeImpl event = new EventTypeImpl();
+	    					event.setId(eventID);
+	    					event.setName(eventID);
+	        				alreadyGen.add(eventID);
+	    				
+	    					app.getEventModel().setEvent(event);
+	    				}
+	    			}
+	    		
+	    		}
+	    		
+	    		for (int j = 0; j < app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getDataFlowGuard().size(); j++) {
+	    			if (app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getDataFlowGuard().get(j).getEventIds() != null) {
+	    				
+	    				String eventID = app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getDataFlowGuard().get(j).getEventIds();
+	    				
+	    				eventCheck.add(eventID);
+
+	    				if (!alreadyGen.contains(eventID)) {
+	    					
+	    					EventTypeImpl event = new EventTypeImpl();
+	    					event.setId(eventID);
+	    					event.setName(eventID);
+	        				alreadyGen.add(eventID);
+	    				
+	    					app.getEventModel().setEvent(event);
+	    				}
+	    			}
+	    		
+	    		}
+	    		
+	    		for (int j = 0; j < app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getFaultLogger().size(); j++) {
+	    			if (app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getFaultLogger().get(j).getEventIds() != null) {
+	    				
+	    				String eventID = app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getFaultLogger().get(j).getEventIds();
+	    				
+	    				eventCheck.add(eventID);
+
+	    				if (!alreadyGen.contains(eventID)) {
+	    					
+	    					EventTypeImpl event = new EventTypeImpl();
+	    					event.setId(eventID);
+	    					event.setName(eventID);
+	        				alreadyGen.add(eventID);
+	    				
+	    					app.getEventModel().setEvent(event);
+	    				}
+	    			}
+	    		
+	    		}
+	    		
+	    		if (app.getComponent().get(0).getGuardedStageModel().getStage().get(i).getSubStage().size() != 0) {
+	    			substageEventID(app.getComponent().get(0).getGuardedStageModel().getStage().get(i), app);
+	    		}
+	    	}
+	    }
+	    
+	    public void substageEventID(EObject stage, CompositeApplicationType app) {
+	    	
+	    	if (stage instanceof StageType) {
+	    		
+	    		int subStages = ((StageType) stage).getSubStage().size();
+	    	
+	    		for (int i = 0; i < subStages; i++) {
+	    			for (int j = 0; j < ((StageType) stage).getSubStage().get(i).getMilestone().size(); j++) {
+	    				if (((StageType) stage).getSubStage().get(i).getMilestone().get(j).getEventIds() != null) {
+	    				
+	    					String eventID = ((StageType)stage).getSubStage().get(i).getMilestone().get(j).getEventIds();
+	    				
+	        				eventCheck.add(eventID);
+
+	    					if (!alreadyGen.contains(eventID)) {
+	    					
+	    						EventTypeImpl event = new EventTypeImpl();
+	    						event.setId(eventID);
+	    						event.setName(eventID);
+	    						alreadyGen.add(eventID);
+	        				
+	    						app.getEventModel().setEvent(event);
+	    				}
+	    			}
+	    		
+	    		}
+	    		
+	    			for (int j = 0; j < ((StageType) stage).getSubStage().get(i).getDataFlowGuard().size(); j++) {
+	    				if (((StageType) stage).getSubStage().get(i).getDataFlowGuard().get(j).getEventIds() != null) {
+	    				
+	    					String eventID = ((StageType) stage).getSubStage().get(i).getDataFlowGuard().get(j).getEventIds();
+	    				
+	        				eventCheck.add(eventID);
+
+	    					if (!alreadyGen.contains(eventID)) {
+	    					
+	    						EventTypeImpl event = new EventTypeImpl();
+	    						event.setId(eventID);
+	    						event.setName(eventID);
+	        					alreadyGen.add(eventID);
+	    				
+	        					app.getEventModel().setEvent(event);
+	    					}
+	    				}
+	    		
+	    			}
+	    			for (int j = 0; j < ((StageType) stage).getSubStage().get(i).getFaultLogger().size(); j++) {
+	    				if (((StageType) stage).getSubStage().get(i).getFaultLogger().get(j).getEventIds() != null) {
+	    				
+	    					String eventID = ((StageType) stage).getSubStage().get(i).getFaultLogger().get(j).getEventIds();
+	    				
+	        				eventCheck.add(eventID);
+
+	    					if (!alreadyGen.contains(eventID)) {
+	    					
+	    						EventTypeImpl event = new EventTypeImpl();
+	    						event.setId(eventID);
+	    						event.setName(eventID);
+	        					alreadyGen.add(eventID);
+	    				
+	        					app.getEventModel().setEvent(event);
+	    					}
+	    				}
+	    		
+	    			}
+	    		
+	    			if (((StageType) stage).getSubStage().get(i).getSubStage().size() != 0) {
+	    				substageEventID(((StageType)stage).getSubStage().get(i), app);
+	    			}
+	    		}
+	    	}
+	    	
+	    	else if (stage instanceof SubStageType) {
+	 
+	    		int subStages = ((SubStageType) stage).getSubStage().size();
+	    	
+	    		for (int i = 0; i < subStages; i++) {
+	    			for (int j = 0; j < ((SubStageType) stage).getSubStage().get(i).getMilestone().size(); j++) {
+	    				if (((SubStageType) stage).getSubStage().get(i).getMilestone().get(j).getEventIds() != null) {
+	    				
+	    					String eventID = ((SubStageType)stage).getSubStage().get(i).getMilestone().get(j).getEventIds();
+	    				
+	        				eventCheck.add(eventID);
+
+	    					if (!alreadyGen.contains(eventID)) {
+	    					
+	    						EventTypeImpl event = new EventTypeImpl();
+	    						event.setId(eventID);
+	    						event.setName(eventID);
+	    						alreadyGen.add(eventID);
+	        				
+	    						app.getEventModel().setEvent(event);
+	    				}
+	    			}
+	    		
+	    		}
+	    		
+	    			for (int j = 0; j < ((SubStageType) stage).getSubStage().get(i).getDataFlowGuard().size(); j++) {
+	    				if (((SubStageType) stage).getSubStage().get(i).getDataFlowGuard().get(j).getEventIds() != null) {
+	    				
+	    					String eventID = ((SubStageType) stage).getSubStage().get(i).getDataFlowGuard().get(j).getEventIds();
+	    				
+	        				eventCheck.add(eventID);
+
+	    					if (!alreadyGen.contains(eventID)) {
+	    					
+	    						EventTypeImpl event = new EventTypeImpl();
+	    						event.setId(eventID);
+	    						event.setName(eventID);
+	        					alreadyGen.add(eventID);
+	    				
+	        					app.getEventModel().setEvent(event);
+	    					}
+	    				}
+	    		
+	    			}
+	    			
+	    			for (int j = 0; j < ((SubStageType) stage).getSubStage().get(i).getFaultLogger().size(); j++) {
+	    				if (((SubStageType) stage).getSubStage().get(i).getFaultLogger().get(j).getEventIds() != null) {
+	    				
+	    					String eventID = ((SubStageType) stage).getSubStage().get(i).getFaultLogger().get(j).getEventIds();
+	    				
+	        				eventCheck.add(eventID);
+
+	    					if (!alreadyGen.contains(eventID)) {
+	    					
+	    						EventTypeImpl event = new EventTypeImpl();
+	    						event.setId(eventID);
+	    						event.setName(eventID);
+	        					alreadyGen.add(eventID);
+	    				
+	        					app.getEventModel().setEvent(event);
+	    					}
+	    				}
+	    		
+	    			}
+	    		
+	    			if (((SubStageType) stage).getSubStage().get(i).getSubStage().size() != 0) {
+	    				substageEventID(((SubStageType)stage).getSubStage().get(i), app);
+	    			}
+	    		}
+	    	}
+	    	
+	    }     
 }
